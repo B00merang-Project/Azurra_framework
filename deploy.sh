5b9c25c909df855d5af9de4b81cf8483d70945d6
Christian Medel <cmedelahumada@gmail.com>
2019-02-05 07:48:35 -0500

    Various MATE fixes, added iOS

--- deploy.sh
+++ deploy.sh
@@ -9,6 +9,10 @@ echo "Building OS X Yosemite"
 cd ../OS_X_10.10
 ./run.sh
 
+echo "Building iOS"
+cd ../iOS_12
+./run.sh
+
 echo "Building Windows 10"
 cd ../Win_10
 ./run.sh
@@ -17,18 +21,18 @@ cd ..
 
 # macOS
 echo "Copying macOS"
-cp -aR macOS_10.14/assets ~/Github/macOS/gtk-3.20
+#cp -aR macOS_10.14/assets ~/Github/macOS/gtk-3.20
 cp -aR macOS_10.14/gtk.css ~/Github/macOS/gtk-3.20
 cp -aR macOS_10.14/gtk-dark.css ~/Github/macOS/gtk-3.20
 cp -aR macOS_10.14/gtk-widgets.css ~/Github/macOS/gtk-3.20
 
-cp -aR macOS_10.14/assets ~/Github/macOS-Dark/gtk-3.20
+#cp -aR macOS_10.14/assets ~/Github/macOS-Dark/gtk-3.20
 cp -aR macOS_10.14/gtk-dark.css ~/Github/macOS-Dark/gtk-3.20/gtk.css
 cp -aR macOS_10.14/gtk-widgets.css ~/Github/macOS-Dark/gtk-3.20
 
 # Windows 10
 echo "Copying Windows 10"
-cp -aR Win_10/assets ~/Github/Windows-10/gtk-3.20
+#cp -aR Win_10/assets ~/Github/Windows-10/gtk-3.20
 cp -aR Win_10/gtk.css ~/Github/Windows-10/gtk-3.20
 cp -aR Win_10/gtk-dark.css ~/Github/Windows-10/gtk-3.20
 cp -aR Win_10/gtk-widgets.css ~/Github/Windows-10/gtk-3.20
@@ -43,3 +47,10 @@ cp -aR OS_X_10.10/assets ~/Github/OS-X-Yosemite/gtk-3.20
 cp -aR OS_X_10.10/gtk.css ~/Github/OS-X-Yosemite/gtk-3.20
 cp -aR OS_X_10.10/gtk-dark.css ~/Github/OS-X-Yosemite/gtk-3.20
 cp -aR OS_X_10.10/gtk-widgets.css ~/Github/OS-X-Yosemite/gtk-3.20
+
+echo "Copying iOS"
+
+#cp -aR iOS_12/assets ~/Github/iOS/gtk-3.20
+cp -aR iOS_12/gtk.css ~/Github/iOS/gtk-3.20
+#cp -aR iOS_12/gtk-dark.css ~/Github/iOS/gtk-3.20
+cp -aR iOS_12/gtk-widgets.css ~/Github/iOS/gtk-3.20
