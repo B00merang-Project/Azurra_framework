998320b1f47662d329ae9b412827fa15d6b5fc61
Christian Medel <cmedelahumada@gmail.com>
2019-02-19 08:04:29 -0500

    Changes to Mavericks, add Leopard

---
+++ OS_X_10.5/deploy.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "Not ready"
+exit
+
+echo "Copying OS X Leopard"
+cp -aR assets ~/Github/OS-X-Leopard/gtk-3.20
+cp -aR gtk.css ~/Github/OS-X-Leopard/gtk-3.20
+cp -aR gtk-widgets.css ~/Github/OS-X-Leopard/gtk-3.20
--- OS_X_10.9/deploy.sh
+++ OS_X_10.9/deploy.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
-# Still in build process
-
 echo "Copying OS X Mavericks"
 cp -aR assets ~/Github/OS-X-Mavericks/gtk-3.20
 cp -aR gtk.css ~/Github/OS-X-Mavericks/gtk-3.20
-#cp -aR gtk-dark.css ~/Github/OS-X-Mavericks/gtk-3.20
 cp -aR gtk-widgets.css ~/Github/OS-X-Mavericks/gtk-3.20
-
