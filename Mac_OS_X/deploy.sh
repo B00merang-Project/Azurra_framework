c7da9fd9540a36be94df401fba75992dd80af957
Christian Medel <cmedelahumada@gmail.com>
2019-02-22 08:50:43 -0500

    Add Mac OS X Cheetah, tweaks to OS X Leopard, print dialog global fix

---
+++ Mac_OS_X/deploy.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Copying Mac OS X Cheetah"
+#cp -aR assets ~/Github/Mac-OS-X-Cheetah/gtk-3.20
+cp -aR gtk.css ~/Github/Mac-OS-X-Cheetah/gtk-3.20
+cp -aR gtk-widgets.css ~/Github/Mac-OS-X-Cheetah/gtk-3.20
--- OS_X_10.5/deploy.sh
+++ OS_X_10.5/deploy.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-echo "Not ready"
-exit
-
 echo "Copying OS X Leopard"
-cp -aR assets ~/Github/OS-X-Leopard/gtk-3.20
+#cp -aR assets ~/Github/OS-X-Leopard/gtk-3.20
 cp -aR gtk.css ~/Github/OS-X-Leopard/gtk-3.20
 cp -aR gtk-widgets.css ~/Github/OS-X-Leopard/gtk-3.20
