daefb3fd5329fe0140d190cd4b5db5a66480ac4c
Christian Medel <cmedelahumada@gmail.com>
2019-03-11 16:06:52 -0400

    Added Vista, small changes

---
+++ Win_6.0_Vista/deploy.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Copying Windows Vista"
+#cp -aR assets ~/Github/Windows-Vista/gtk-3.20
+cp -aR gtk.css ~/Github/Windows-Vista/gtk-3.20
+cp -aR gtk-widgets.css ~/Github/Windows-Vista/gtk-3.20
--- Win_7/deploy.sh
+++ Win_7/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 echo "Copying Windows 7"
-cp -aR assets ~/Github/Windows-7/gtk-3.0
+#cp -aR assets ~/Github/Windows-7/gtk-3.0
 cp -aR gtk.css ~/Github/Windows-7/gtk-3.0
 cp -aR gtk-widgets.css ~/Github/Windows-7/gtk-3.0
