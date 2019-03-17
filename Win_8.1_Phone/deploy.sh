0cabd6e7cd9efa958f48a401b281dff48e43d939
Christian Medel <cmedelahumada@gmail.com>
2019-03-17 12:00:27 -0400

    Contrast in dark themes, color changes & added Windows Phone 8.1

---
+++ Win_8.1_Phone/deploy.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "Copying Windows Phone 8.1"
+
+#cp -aR assets ~/Github/Windows-Phone-8.1/gtk-3.0
+cp -aR gtk.css ~/Github/Windows-Phone-8.1/gtk-3.0
+cp -aR gtk-dark.css ~/Github/Windows-Phone-8.1/gtk-3.0
+cp -aR gtk-widgets.css ~/Github/Windows-Phone-8.1/gtk-3.0
