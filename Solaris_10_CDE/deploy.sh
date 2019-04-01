5e26fdc9a27178dda944eb0f80c48f6d5bf666f9
Christian Medel <cmedelahumada@gmail.com>
2019-03-31 21:45:17 -0400

    Changes to scrollbars, added Mac OS 9

---
+++ Solaris_10_CDE/deploy.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "Not ready"
+exit
+
+echo "Copying Windows 95"
+#cp -aR assets ~/Github/Windows-95/gtk-3.0
+cp -aR gtk.css ~/Github/Windows-95/gtk-3.0
+cp -aR gtk-widgets.css ~/Github/Windows-95/gtk-3.0
