8ffeff58e999938f6fbfa101f7ebc6c11446c132
Christian Medel <cmedelahumada@gmail.com>
2019-04-04 09:31:20 -0400

    Add B00merang Glass, Green and Dark, tweaks to Flat

---
+++ B00merang/green/deploy.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Copying B00merang Green"
+#cp -aR assets ~/Github/B00merang-Themes/B00merang\ Green/gtk-3.20
+cp -aR gtk.css ~/Github/B00merang-Themes/B00merang\ Green/gtk-3.20
+cp -aR gtk-widgets.css ~/Github/B00merang-Themes/B00merang\ Green/gtk-3.20
