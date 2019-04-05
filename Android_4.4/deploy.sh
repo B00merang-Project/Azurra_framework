8feb56d7f4aff6ec9ae48df8f2f68e29c242dbd9
Christian Medel <cmedelahumada@gmail.com>
2019-04-05 09:57:15 -0400

    Added Android 4.4

---
+++ Android_4.4/deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Copying Android 4.4 Kitkat"
+#cp -aR assets ~/Github/Android-4.4-Kitkat/gtk-3.0
+cp -aR gtk.css ~/Github/Android-4.4-Kitkat/gtk-3.0
+cp -aR gtk-dark.css ~/Github/Android-4.4-Kitkat/gtk-3.0
+cp -aR gtk-widgets.css ~/Github/Android-4.4-Kitkat/gtk-3.0
+
+#cp -aR assets ~/Github/Android-4.4-Kitkat-Dark/gtk-3.0
+cp -aR gtk-dark.css ~/Github/Android-4.4-Kitkat-Dark/gtk-3.0/gtk.css
+cp -aR gtk-widgets.css ~/Github/Android-4.4-Kitkat-Dark/gtk-3.0
---
+++ B00merang/round/deploy.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Copying B00merang Round"
+#cp -aR assets ~/Github/B00merang-Themes/B00merang\ Round/gtk-3.20
+cp -aR gtk.css ~/Github/B00merang-Themes/B00merang\ Round/gtk-3.20
+cp -aR gtk-widgets.css ~/Github/B00merang-Themes/B00merang\ Round/gtk-3.20
