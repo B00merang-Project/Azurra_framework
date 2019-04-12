f2adafc27ab07c99452c62fafac1b2ec04e48fc3
Christian Medel <cmedelahumada@gmail.com>
2019-04-12 11:25:02 -0400

    Retool build system, rename Round to Circle

---
+++ B00merang/circle/deploy.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Copying B00merang Circle"
+#cp -aR assets ~/Github/B00merang-Circle/gtk-3.20
+cp -aR gtk.css ~/Github/B00merang-Circle/gtk-3.20
+cp -aR gtk-widgets.css ~/Github/B00merang-Circle/gtk-3.20
--- B00merang/dark/deploy.sh
+++ B00merang/dark/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 echo "Copying B00merang Dark"
-#cp -aR assets ~/Github/B00merang-Themes/B00merang\ Dark/gtk-3.20
-cp -aR gtk.css ~/Github/B00merang-Themes/B00merang\ Dark/gtk-3.20
-cp -aR gtk-widgets.css ~/Github/B00merang-Themes/B00merang\ Dark/gtk-3.20
+#cp -aR assets ~/Github/B00merang-Dark/gtk-3.20
+cp -aR gtk.css ~/Github/B00merang-Dark/gtk-3.20
+cp -aR gtk-widgets.css ~/Github/B00merang-Dark/gtk-3.20
