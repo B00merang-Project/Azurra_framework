33414e96810e2bd426ea6c3367a381e11f7b105d
Christian Medel <cmedelahumada@gmail.com>
2019-06-06 16:26:37 -0400

    Prepare for Longhorn & Whistler port, add TODO

---
+++ Win_5.3_Longhorn/slate/deploy.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Copying Windows XP Luna"
+#cp -aR assets ~/Github/Windows-XP/Windows\ XP\ Luna/gtk-3.20
+cp -aR gtk.css ~/Github/Windows-XP/Windows\ XP\ Luna/gtk-3.20
+cp -aR gtk-widgets.css ~/Github/Windows-XP/Windows\ XP\ Luna/gtk-3.20
