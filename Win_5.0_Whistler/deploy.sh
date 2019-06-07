bdae04e729c2e8152f69cacd0d14e9a804351e39
Christian Medel <cmedelahumada@gmail.com>
2019-06-07 10:16:49 -0400

    fix color issues & inconsistencies, port Whistler

--- Win_5.0_Whistler/deploy.sh
+++ Win_5.0_Whistler/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "Copying Windows 95"
-#cp -aR assets ~/Github/Windows-95/gtk-3.0
-cp -aR gtk.css ~/Github/Windows-95/gtk-3.0
-cp -aR gtk-widgets.css ~/Github/Windows-95/gtk-3.0
+echo "Copying Windows Whistler"
+#cp -aR assets ~/Github/Windows-Whistler/gtk-3.20
+cp -aR gtk.css ~/Github/Windows-Whistler/gtk-3.20
+cp -aR gtk-widgets.css ~/Github/Windows-Whistler/gtk-3.20
