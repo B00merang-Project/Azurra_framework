fe6025342858e09cf17cfb52c50137231a2ab4d8
Christian Medel <cmedelahumada@gmail.com>
2019-06-11 16:58:56 -0400

    Start Haiku Beta 1

---
+++ Haiku_B1/deploy.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "Not ready"
+exit
+
+echo "Copying BeOS R5"
+#cp -aR assets ~/Github/BeOS-R5/gtk-3.0
+cp -aR gtk.css ~/Github/BeOS-R5/gtk-3.0
+cp -aR gtk-widgets.css ~/Github/BeOS-R5/gtk-3.0
