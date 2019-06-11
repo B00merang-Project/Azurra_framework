e7342e247a704e40fb693dfdf9f866e86182335b
Christian Medel <cmedelahumada@gmail.com>
2019-06-11 13:30:05 -0400

    Small corrections, added tvOS

---
+++ tvOS/deploy.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "WAY NOT READY!"
+exit
+
+echo "Copying watchOS"
+#cp -aR assets ~/Github/watchOS/gtk-3.0
+cp -aR gtk.css ~/Github/watchOS/gtk-3.0
+cp -aR gtk-widgets.css ~/Github/watchOS/gtk-3.0
