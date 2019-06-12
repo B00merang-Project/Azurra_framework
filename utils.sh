468343ca3a274e620dec3b9c00dcb16959544138
Christian Medel <cmedelahumada@gmail.com>
2019-06-12 16:56:25 -0400

    New argument processing

--- utils.sh
+++ utils.sh
@@ -7,87 +7,62 @@
 version=0.1
 description="Azurra Utils, version $version"
 
-# Operation codes
-OP_EXIT=0
-OP_NEW=1
-OP_PARENTS=2
-OP_CHILDREN=3
-
 # Exit codes
-CODE_OK=0
-CODE_FAIL=1
-CODE_UNKOWN=2
+OK=0
+ERROR=1
+INVALID_ARG=2
 
 #############################################
 # FUNCTIONS
 #############################################
+show_help() {
+  echo $description
+  echo
 
-TARGET=0
-OP=0
-WIDGETS=0
-ALL=0
+  echo "  -h   --help         " "Shows help"
+  echo "  -v   --version      " "Script version"
+  echo "  -p   --parents      " "List of widgets inherited from other themes Requires <target>"
+  echo "  -c   --children     " "List of themes using resources from a theme. Requires <target>"
+  echo "  -a   --all          " "Use with -p or -c, do operation for all themes"
+  echo "  -w   --widget       " "Use with -p or -c, restricts search to <widget>"
+  echo "  -n   --new          " "Initialise a new theme directory. Requires <name> and <parent> (Azurra by default)"
 
-for arg in "$@"
-do
-  case $arg in
-    -p|--parents)
-    ;;
-    
-    -c|--children)
-    ;;
-    
-    -w|--widget)
-    ;;
-    
-    -n|--new)
-    
-    ;;
-  
-    -h|--help)
-      echo $description
-      echo
-    
-      echo "  -h   --help         " "Shows help"
-      echo "  -v   --version      " "Script version"
-      echo "  -p   --parents      " "List of widgets inherited from other themes Requires <target>"
-      echo "  -c   --children     " "List of themes using resources from a theme. Requires <target>"
-      echo "  -a   --all          " "Use with -p or -c, do operation for all themes"
-      echo "  -w   --widget       " "Use with -p or -c, restricts search to <widget>"
-      echo "  -n   --new          " "Initialise a new theme directory. Requires <name> and <parent> (Azurra by default)"
+  echo
+  echo "More information: <http://github.com/Azurra_Utils/wiki>"
+}
 
-      echo
-      echo "More information: <http://github.com/Azurra_Utils/wiki>"
-      
-      OP=$OP_EXIT
-    ;;
-    
-   -v|--version)
-      echo $description
-      echo "Copyright (c) 2019 The B00merang Group"
-      echo "License GPLv3+: GNU GPL version 3 or later <http://gnu.org/licenses/gpl.html>"
-      
-      OP=$OP_EXIT
-    ;;
-    
-    -*)
-      echo "Unknown action: $arg"
-      # interrupt
-      exit
-    ;;
+show_version() {
+  echo $description
+  echo "Copyright (c) 2019 The B00merang Group"
+  echo "License GPLv3+: GNU GPL version 3 or later <http://gnu.org/licenses/gpl.html>"
+}
+
+invalid() {
+  echo "Invalid argument '$1'"
+}
+
+while [ "$1" != "" ]; do
+  case $1 in
+    -p | --parents )        shift
+                            parent=$1
+                            ;;
+    -c | --children )       shift
+                            children=$1
+                            ;;
+    -w | --widget )         shift
+                            children=$1
+                            ;;
+    -n | --new )            shift
+                            name=$1
+                            ;;
+    -h | --help )           show_help
+                            exit $OK
+                            ;;
+    -v | --version )        show_version
+                            exit $OK
+                            ;;
+    * )                     invalid $1
+                            exit $INVALID_ARG
   esac
+  shift
 done
-
-case $OP in
-  "$OP_EXIT")
-    exit $CODE_OK
-  ;;
-  "$OP_NEW")
-  
-  ;;
-  "$OP_PARENT")
-  
-  ;;
-  "$OP_CHILDREN")
-  
-  ;;
-esac
