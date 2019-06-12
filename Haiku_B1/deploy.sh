df49a906482dc6837c9a0c555a105d4b5d417e3b
Christian Medel <cmedelahumada@gmail.com>
2019-06-11 20:36:36 -0400

    Tweak BeOS, complete Haiku

--- Haiku_B1/deploy.sh
+++ Haiku_B1/deploy.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-echo "Not ready"
-exit
-
-echo "Copying BeOS R5"
-#cp -aR assets ~/Github/BeOS-R5/gtk-3.0
-cp -aR gtk.css ~/Github/BeOS-R5/gtk-3.0
-cp -aR gtk-widgets.css ~/Github/BeOS-R5/gtk-3.0
+echo "Copying Haiku"
+#cp -aR assets ~/Gihub/Haiku/gtk-3.0
+cp -aR gtk.css ~/Github/Haiku/gtk-3.0
+cp -aR gtk-widgets.css ~/Github/Haiku/gtk-3.0
