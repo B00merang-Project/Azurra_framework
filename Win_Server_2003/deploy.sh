33bf9db35c84042d836e65c8d26593244484d844
Christian Medel <cmedelahumada@gmail.com>
2019-06-12 11:40:52 -0400

    Smal tweaks to Windows 8.1, added Windows Server 2003/2012 R2

---
+++ Win_Server_2003/deploy.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Copying Windows Server 2003"
+#cp -aR assets ~/Github/Windows-Server-2003/gtk-3.0
+cp -aR gtk.css ~/Github/Windows-Server-2003/gtk-3.0
+cp -aR gtk-widgets.css ~/Github/Windows-Server-2003/gtk-3.0
---
+++ Win_Server_2012_R2/deploy.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Copying Windows Server 2012 R2"
+#cp -aR assets ~/Github/Windows-Server-2012-R2/gtk-3.0
+cp -aR gtk.css ~/Github/Windows-Server-2012-R2/gtk-3.0
+cp -aR gtk-widgets.css ~/Github/Windows-Server-2012-R2/gtk-3.0
