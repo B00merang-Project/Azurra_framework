e9e608e04c4978ba75d992c7b6f650b83c95e4e6
Christian Medel <cmedelahumada@gmail.com>
2019-06-14 10:35:05 -0400

    Android now fully compliant with new structure

--- Android/build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-sass -C --sourcemap=none _gtk-dark.scss gtk-dark.css
-
-sass -C -q --sourcemap=none _common.scss gtk.css
--- Android/deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "Copying Android"
-#cp -aR assets ~/Github/Android/gtk-3.20
-cp -aR gtk.css ~/Github/Android/gtk-3.20
-cp -aR gtk-dark.css ~/Github/Android/gtk-3.20
-cp -aR gtk-widgets.css ~/Github/Android/gtk-3.20
