e995690c02580b63fe1b0b0b0498c4b0b3743690
Christian Medel <cmedelahumada@gmail.com>
2019-06-17 10:47:44 -0400

    Revert "Start migration to pure SASS"
    This reverts commit a39e0648452e63b8a6e6b0b0eb7a42d784324424.

---
+++ B00merang/brome/deploy.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Copying B00merang Brome"
+#cp -aR assets ~/Github/B00merang-Brome/gtk-3.0
+cp -aR gtk.css ~/Github/B00merang-Brome/gtk-3.0
+cp -aR gtk-widgets.css ~/Github/B00merang-Brome/gtk-3.0
