16391c83c8cd4a2ac2868c4ea735c0ae3225b4cb
Christian Medel <cmedelahumada@gmail.com>
2019-06-17 11:09:24 -0400

    Migrate Android TV, Android & Android 4.4 color changes, Adapt GTK functions for better result

--- B00merang/brome/deploy.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo "Copying B00merang Brome"
-#cp -aR assets ~/Github/B00merang-Brome/gtk-3.0
-cp -aR gtk.css ~/Github/B00merang-Brome/gtk-3.0
-cp -aR gtk-widgets.css ~/Github/B00merang-Brome/gtk-3.0
