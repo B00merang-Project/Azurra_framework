a3cdeb1319e7cd14f318ec530c6c913e76337e2a
Christian Medel <cmedelahumada@gmail.com>
2019-06-17 20:25:40 -0400

    Migrate Chrome OS, Fuchsia, Haiku & small tweaks

--- Fuchsia/deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "Copying Fuchsia"
-#cp -aR assets ~/Github/Fuchsia/gtk-3.0
-cp -aR gtk.css ~/Github/Fuchsia/gtk-3.0
-cp -aR gtk-dark.css ~/Github/Fuchsia/gtk-3.0
-cp -aR gtk-widgets.css ~/Github/Fuchsia/gtk-3.0
--- Haiku_B1/deploy.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo "Copying Haiku"
-#cp -aR assets ~/Gihub/Haiku/gtk-3.0
-cp -aR gtk.css ~/Github/Haiku/gtk-3.0
-cp -aR gtk-widgets.css ~/Github/Haiku/gtk-3.0
