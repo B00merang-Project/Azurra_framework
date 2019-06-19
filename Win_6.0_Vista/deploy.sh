87e50d0b2e39b9104b9d8738ba81906d96a8375a
Christian Medel <cmedelahumada@gmail.com>
2019-06-18 22:26:43 -0400

    Migrate Windows 10, 8.1, 7, Vista, Server 2012 & increase Xfce support

--- Win_6.0_Vista/deploy.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo "Copying Windows Vista"
-#cp -aR assets ~/Github/Windows-Vista/gtk-3.20
-cp -aR gtk.css ~/Github/Windows-Vista/gtk-3.20
-cp -aR gtk-widgets.css ~/Github/Windows-Vista/gtk-3.20
