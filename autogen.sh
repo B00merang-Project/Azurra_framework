3dcac871c203b6803fec3c5626871d0d5f3bbb89
Christian Medel <cmedelahumada@gmail.com>
2019-07-24 11:28:49 -0400

    Minor file changes

--- autogen.sh
+++ autogen.sh
@@ -11,11 +11,10 @@ ROOT_DIR="$PWD"
 
 show_help() {
   echo -e "$description\n"
-  
-  echo -e "Usage:  ./autogen.sh <ARGUMENTS> <TARGETS>\n"
-  
   echo -e "Generates and deploys CSS and asset files\n"
-  
+
+  echo -e "Usage:  ./autogen.sh <ARGUMENTS> <TARGETS>\n"
+
   echo "  -h   --help         " "Shows help"
   echo "  -v   --version      " "Script version"
   echo "  -q   --quiet        " "Silences ALL SASS warnings"
@@ -24,7 +23,7 @@ show_help() {
   echo "  -c   --compile      " "Run SASS compiler only (no deployment)"
   echo "  -r   --render       " "Run asset generation script if found"
   echo "  -e   --empty-assets " "Removes previously rendered assets. Run before rendering after changes"
-  
+
   echo -e "\nMore information: <http://github.com/Elbullazul/Azurra_framework/wiki>" && exit
 }
 
@@ -36,7 +35,7 @@ show_version() {
 
 fail() { tput setaf 1 && echo 'ERROR: '$@ && exit 1; }
 warn() { tput setaf 220 && echo 'WARNING: '$@ && tput sgr 0; }
-highlight() { tput setaf 33 && echo $@ && tput sgr 0; }
+hlight() { tput setaf 33 && echo $@ && tput sgr 0; }
 
 is_theme() { [ -f "$1/theme.conf" ] && return 0 || return 1; }
 is_bundle() { [ -f "$1/bundle.conf" ] && return 0 || return 1; }
@@ -50,7 +49,7 @@ has_render() {
 
 recursive() {
   for dir in $@; do
-    is_theme $dir && $OP "$dir" || is_bundle $dir && recursive "$dir"/*
+    is_theme $dir && $FUNC "$dir" || is_bundle $dir && recursive "$dir"/*
   done
 }
 
@@ -58,22 +57,22 @@ deploy() {
   load_conf $1
   [ -z "$target_dir" ] && warn "Empty target for $name, skipping deployment" && return
 
-  echo -n "Deploying $(highlight -n $name), "
-  
+  echo -n "Deploying $(hlight -n $name), "
+
   # Stylesheets
   rm -rf "$target_dir"/*.css
   has_dark && rm -rf "$target_dir_dark"/*.css
   has_light && rm -rf "$target_dir_light"/*.css
-  
+
   cp $1/gtk*.css "$target_dir"
   has_dark && cp $1/gtk-dark.css "$target_dir_dark/gtk.css"
   has_light && cp $1/gtk-light.css "$target_dir_light/gtk.css"
-  
+
   # Assets
   rm -rf "$target_dir/assets"
   has_dark && rm -rf "$target_dir_dark/assets"
   has_light && rm -rf "$target_dir_light/assets"
-  
+
   cp -r $1/assets "$target_dir"
   if has_dark; then
     [ -d $1/assets-dark ] && cp -r $1/assets-dark "$target_dir_dark/assets" || cp -r $1/assets "$target_dir_dark"
@@ -81,33 +80,33 @@ deploy() {
   if has_light; then
     [ -d $1/assets-dark ] && cp -r $1/assets-light "$target_dir_light/assets" || cp -r $1/assets "$target_dir_light"
   fi
-  
+
   echo 'done'
 }
 
 compile() {
   load_conf $1
-  [ $LOCK_ADD == true ] && echo -n "Compiling $(highlight -n $name), " || echo "Compiling $(highlight $name)"
+  [ $LOCK_ADD == true ] && echo -n "Compiling $(hlight -n $name), " || echo "Compiling $(hlight $name)"
 
   for sass_file in $1/gtk*.scss; do
     local filename=${sass_file%".scss"}
     sass $sass_args $sass_file $filename.css
-    
+
     [ $? -ne 0 ] && fail "SASS exited unexpectedly, aborting"
   done
-  
+
   echo 'done'
 }
 
 render() {
   has_render $1 || warn "$1 has no asset generation module"
   ! has_render $1 && return
-    
+
   # run
   [ -d "$1/assets-render" ] && (cd "$1/assets-render" && ./render-assets.sh && cd ..)
   [ -d "$1/assets-render-dark" ] && (cd "$1/assets-render-dark" && ./render-assets.sh && cd ..)
   [ -d "$1/assets-render-light" ] && (cd "$1/assets-render-light" && ./render-assets.sh && cd ..)
-  
+
   # local deploy
   cp -R "$1/assets-render/assets/"* "$1/assets"
   [ -d "$1/assets-render-dark" ] && cp -R "$1/assets-render-dark/assets/"* "$1/assets-dark"
@@ -117,7 +116,7 @@ render() {
 empty_assets() {
   has_render $1 || warn "$1 has no asset generation module"
   ! has_render $1 && return
-  
+
   # run
   [ -d "$1/assets-render" ] && rm -rf "$1/assets-render/assets/"*
   [ -d "$1/assets-render-dark" ] && rm -rf "$1/assets-render-dark/assets/"*
@@ -127,12 +126,12 @@ empty_assets() {
 make() {
   compile $1
   deploy $1
-  
+
   echo
 }
 
 # Program vars
-OP="make"
+FUNC="make"
 LOCK_ADD=false
 sass_args="-C --sourcemap=none"
 
@@ -146,13 +145,13 @@ while [ "$1" != "" ]; do
                             ;;
     -q | --quiet )          sass_args="$sass_args -q"
                             ;;
-    -c | --compile )        OP='compile'
+    -c | --compile )        FUNC='compile'
                             ;;
-    -d | --deploy )         OP='deploy'
+    -d | --deploy )         FUNC='deploy'
                             ;;
-    -r | --render )         OP='render'
+    -r | --render )         FUNC='render'
                             ;;
-    -e | --empty-assets )   OP='empty_assets'
+    -e | --empty-assets )   FUNC='empty_assets'
                             ;;
     -h | --help )           show_help
                             ;;
