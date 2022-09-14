cc2d91c6d0db7d9e567c9db4a4cfe9d51a9d34a8
Christian Medel <cmedelahumada@gmail.com>
2022-09-13 20:30:46 -0400

    Remove unported themes (will be added back one by one)

--- windows6-slate/deploy.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo "Copying Windows Longhorn Slate"
-#cp -aR assets ~/Github/Windows-Longhorn/Windows\ Longhorn\ Slate/gtk-3.20
-cp -aR gtk.css ~/Github/Windows-Longhorn/Windows\ Longhorn\ Slate/gtk-3.20
-cp -aR gtk-widgets.css ~/Github/Windows-Longhorn/Windows\ Longhorn\ Slate/gtk-3.20
